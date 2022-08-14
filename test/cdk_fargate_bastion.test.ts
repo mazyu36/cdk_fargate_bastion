b98d78f40de61bae33d42031c851f7bd52dfb1bd
maz <yiyth.fcb6@gmail.com>
2022-08-14 16:21:39 +0900

    Initial commit

---
+++ test/cdk_fargate_bastion.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkFargateBastion from '../lib/cdk_fargate_bastion-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_fargate_bastion-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkFargateBastion.CdkFargateBastionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
